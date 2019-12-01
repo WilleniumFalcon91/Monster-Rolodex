6bd311f5e75d09f2a6cff01b91a544e3e2761b2b
William Elliott <wcelliott1991@gmail.com>
2019-12-01 18:46:52 -0500

    abstracts searchbox and handleChange logic to filter search results

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import CardList from "./components/card-list";
+import SearchBox from "./components/search-box"
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -16,12 +18,24 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }))
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className="App">
         <header className="App-header">
-          <CardList monsters={this.state.monsters} />
+          <h1>Monsters Rolodex</h1>
+          <SearchBox
+            placeholder="search monsters"
+            handleChange={this.handleChange}
+          />
+
+          <br />
+          <CardList monsters={filteredMonsters} />
         </header>
       </div>
     );
